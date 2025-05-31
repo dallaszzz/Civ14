477ce7e5a3ad3c856fbd86f66a8f665449303908
dallaszzz <96160322+dallaszzz@users.noreply.github.com>
2025-05-31 12:04:15 -0400

    Some next form of this stupid code
    I hate it

--- Content.Client/Civ14/LookZoom/LookZoomComponent.cs
+++ Content.Client/Civ14/LookZoom/LookZoomComponent.cs
@@ -1,7 +1,12 @@
+using Content.Shared._Stalker.Lay;
+
 namespace Content.Client.Civ14.LookZoom;
 
 [RegisterComponent]
 public sealed partial class LookZoomComponent : Component
 {
-
+    [DataField]
+    public bool State = false;
+    [DataField]
+    public TimeSpan CoolDown = TimeSpan.Zero;
 }
--- Content.Client/Civ14/LookZoom/LookZoomSystem.cs
+++ Content.Client/Civ14/LookZoom/LookZoomSystem.cs
@@ -1,29 +1,71 @@
+using Content.Shared.Camera;
+using Content.Shared.Hands;
 using Content.Shared.Input;
+using Content.Shared.Movement.Components;
+using Robust.Shared.Input;
 using Robust.Shared.Input.Binding;
+using Content.Client.Movement.Components;
+using Content.Shared.Wieldable.Components;
+using Content.Client.Movement.Systems;
+using Robust.Client.Player;
+using Content.Shared.Inventory;
+using Robust.Shared.Player;
+using Robust.Shared.Containers;
+using Robust.Shared.GameObjects;
+using Content.Shared.Hands.EntitySystems;
+using Content.Shared.Hands.Components;
+using Robust.Shared.Timing;
 
 
 namespace Content.Client.Civ14.LookZoom;
 public sealed class LookZoomSystem : EntitySystem
 {
+    [Dependency] private readonly InventorySystem _inventorySystem = default!;
+    [Dependency] private readonly EyeCursorOffsetSystem _eyeOffset = default!;
+    [Dependency] private readonly IPlayerManager _player = default!;
+    [Dependency] private readonly SharedHandsSystem _handsSystem = default!;
+    [Dependency] private readonly IGameTiming _timing = default!;
     public override void Initialize()
     {
         base.Initialize();
 
+        SubscribeLocalEvent<EyeCursorOffsetComponent, HeldRelayedEvent<GetEyeOffsetRelayedEvent>>(UpdateLookZoom);
+
         CommandBinds.Builder
-        .Bind(ContentKeyFunctions.LookZoom, new PointerInputCmdHandler(OnLookZoom))
+        .Bind(ContentKeyFunctions.LookZoom, InputCmdHandler.FromDelegate(OnLookZoomHandler, handle: false, outsidePrediction: false))
         .Register<LookZoomSystem>();
     }
 
-    private bool OnLookZoom(in PointerInputCmdHandler.PointerInputCmdArgs args)
+    public void OnLookZoomHandler(ICommonSession? session)
     {
-        if (args.Session == null)
-            return false;
-        var session = args.Session;
-        var entity = session.AttachedEntity;
+        var entity = session?.AttachedEntity;
+        if (entity is null)
+            return;
 
         if (!TryComp<LookZoomComponent>(entity, out var comp))
-            return false;
+            return;
 
-        return true;
+        if (comp.CoolDown >= _timing.CurTime)
+            return;
+
+        comp.State = !comp.State;
+        comp.CoolDown = _timing.CurTime + TimeSpan.FromSeconds(0.5);
+        return;
+
+    }
+    public void UpdateLookZoom(Entity<EyeCursorOffsetComponent> entity, ref HeldRelayedEvent<GetEyeOffsetRelayedEvent> args)
+    {
+        var playerUid = _player.LocalEntity;
+        if (!TryComp<LookZoomComponent>(playerUid, out var comp))
+            return;
+
+        if (comp.State == false)
+            return;
+
+        var offset = _eyeOffset.OffsetAfterMouse(entity.Owner, null);
+        if (offset == null)
+            return;
+
+        args.Args.Offset += offset.Value;
     }
 }
