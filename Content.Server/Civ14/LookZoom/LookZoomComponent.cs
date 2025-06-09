1afda9c0aa5f645c8888995ad5540e485edd69cb
dallaszzz <96160322+dallaszzz@users.noreply.github.com>
2025-06-09 19:08:31 -0400

    Gives these components to humans and some related cleanup

--- Content.Server/Civ14/LookZoom/LookZoomComponent.cs
+++ Content.Server/Civ14/LookZoom/LookZoomComponent.cs
@@ -3,7 +3,4 @@ namespace Content.Client.Civ14.LookZoom;
 [RegisterComponent]
 public sealed partial class LookZoomComponent : Component
 {
-    public bool State = false;
-
-    public TimeSpan DelayedTime;
 }
